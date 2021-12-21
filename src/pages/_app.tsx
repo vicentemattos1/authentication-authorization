0f17a3dc8418bf3279a32e593196462946fe1ebc
Vicente Mattos <vicentemattos1@hotmail.com>
2021-12-21 18:32:47 -0300

    Inicialização do projeto e adição do contexto de auth

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app";
+import { AuthProvider } from "../contexts/AuthContext";
+import "../styles/global.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
